5cbd6edf5b5736d4302d09a6f5276b3b3823a0b0
Zubair Mohsin <zubairmohsin33@gmail.com>
2021-06-20 19:25:24 +0500

    Adds page for all downloadable assets

---
+++ app/Http/Controllers/ProjectsAssetsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interfaces\AssetEloquentInterface;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
+
+class ProjectsAssetsController extends Controller
+{
+    /**
+     * @var AssetEloquentInterface
+     */
+    protected $assets;
+
+    /**
+     * ProjectsController constructor.
+     *
+     * @param ProjectEloquentInterface $projects
+     */
+    public function __construct(AssetEloquentInterface $assets)
+    {
+        $this->assets = $assets;
+    }
+
+    public function index()
+    {
+        $assets = $this->assets->paginate('created_at')
+            ->map(function ($asset) {
+                return (object)[
+                    'id' => $asset->id,
+                    'path' => $asset->path,
+                    'size' => Storage::size($asset->path),
+                    'url' => Storage::url($asset->path),
+                    'mimeType' => Storage::mimeType($asset->path),
+                    'lastModified' => Carbon::createFromTimestampUTC(
+                        Storage::lastModified($asset->path)
+                    )->diffForHumans(),
+                ];
+            });
+
+        return view('projects.assets.index', compact('assets'));
+    }
+}
